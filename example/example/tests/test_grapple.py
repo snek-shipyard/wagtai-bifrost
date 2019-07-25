706438546a08a3dfdcd728e22f844b4f1f5419b1
Cameron Lamb <cameron.lamb@torchbox.com>
2019-07-25 17:50:11 +0100

    Add initial tests for blog

--- example/example/tests/test_grapple.py
+++ example/example/tests/test_grapple.py
@@ -11,11 +11,12 @@ from wagtail.core.models import Page
 SCHEMA = locate(settings.GRAPHENE['SCHEMA'])
 
 
-class BifrostTest(TestCase):
+class BaseBifrostTest(TestCase):
     def setUp(self):
         self.client = Client(SCHEMA)
 
 
+class PagesTest(BaseBifrostTest):
     def test_pages(self):
         query = '''
         {
@@ -28,7 +29,8 @@ class BifrostTest(TestCase):
         executed = self.client.execute(query)
 
         self.assertEquals(type(executed['data']), OrderedDict)
-        self.assertEquals(type(executed['data']['pages']), OrderedDict)
+        self.assertEquals(type(executed['data']['pages']), list)
+        self.assertEquals(type(executed['data']['pages'][0]), OrderedDict)
 
         pages = Page.objects.all()
 
