3ee6422f973d95a9647a7f98dfc27042224fea35
Cameron Lamb <cameron.lamb@torchbox.com>
2019-07-26 16:38:56 +0100

    Fix the generation of the streamfield content

--- example/example/tests/test_blog.py
+++ example/example/tests/test_blog.py
@@ -4,7 +4,7 @@ import decimal
 import wagtail_factories
 from home.blocks import ImageGalleryImages, ImageGalleryImage
 from home.factories import BlogPageFactory, ImageGalleryImageFactory
-from wagtail.core.blocks import StreamValue
+from wagtail.core.blocks import StreamValue, StructValue, BoundBlock
 from wagtail.core.rich_text import RichText
 
 from .test_bifrost import BaseBifrostTest
@@ -13,6 +13,7 @@ from .test_bifrost import BaseBifrostTest
 class BlogTest(BaseBifrostTest):
     def setUp(self):
         super().setUp()
+
         self.blog_page = BlogPageFactory(
             body=[
                 ("heading", "Test heading 1"),
@@ -25,14 +26,20 @@ class BlogTest(BaseBifrostTest):
                 ("gallery", {
                     "title": "Gallery title",
                     "images": StreamValue(
-                        stream_block=ImageGalleryImages,
+                        stream_block=ImageGalleryImages(),
                         stream_data=[
                             (
-                                'image',
-                                wagtail_factories.ImageFactory(),
+                                "image", {
+                                    "image": wagtail_factories.ImageChooserBlockFactory(),
+                                },
+                            ),
+                            (
+                                "image", {
+                                    "image": wagtail_factories.ImageChooserBlockFactory(),
+                                },
                             ),
                         ],
-                    )
+                    ),
                 }),
             ],
         )
