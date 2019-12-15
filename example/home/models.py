yashttm/wagtail-grapple

implement `register_query_field`

example/home/models.py
@@ -19,6 +19,7 @@ from wagtail.documents.edit_handlers import DocumentChooserPanel
 from wagtail_headless_preview.models import HeadlessPreviewMixin
 from wagtailmedia.edit_handlers import MediaChooserPanel
 
+from grapple.helpers import register_query_field
 from grapple.models import (
     GraphQLField,
     GraphQLString,
@@ -157,6 +158,7 @@ class Author(Orderable):
 
 
 @register_snippet
+@register_query_field('advert')
 class Advert(models.Model):
     url = models.URLField(null=True, blank=True)
     text = models.CharField(max_length=255)

snek-shipyard/wagtai-bifrost

example/home/models.py
@@ -19,6 +19,7 @@ from wagtail.documents.edit_handlers import DocumentChooserPanel
 from wagtail_headless_preview.models import HeadlessPreviewMixin
 from wagtailmedia.edit_handlers import MediaChooserPanel
 
+from bifrost.helpers import register_query_field
 from bifrost.models import (
     GraphQLField,
     GraphQLString,
@@ -159,6 +160,7 @@ class Author(Orderable):
 
 
 @register_snippet
+@register_query_field('advert')
 class Advert(models.Model):
     url = models.URLField(null=True, blank=True)
     text = models.CharField(max_length=255)