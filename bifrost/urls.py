25def9562232a7ca84a7fd169a80edee9321d152
schettn <nicoschett@icloud.com>
2021-01-11 22:08:33 +0100

    Repalce graphiql with playground
    Replace graphiql with playground; a new interface.
    The views were really messy and therefore I implemented
    them in a better way.

--- bifrost/urls.py
+++ bifrost/urls.py
@@ -1,33 +1,16 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.shortcuts import render
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from graphene_file_upload.django import FileUploadGraphQLView
 from graphene_subscriptions.consumers import GraphqlSubscriptionConsumer
 
-
-def graphiql(request):
-    graphiql_settings = {
-        "REACT_VERSION": "16.13.1",
-        "GRAPHIQL_VERSION": "0.17.5",
-        "SUBSCRIPTIONS_TRANSPORT_VERSION": "0.9.16",
-        "subscriptionsEndpoint": "ws://localhost:8000/subscription/",
-        "endpointURL": "/graphql",
-    }
-
-    return render(request, "bifrost/graphiql.html", graphiql_settings)
-
+from .views import BifrostView
 
 # Traditional URL routing
-SHOULD_EXPOSE_GRAPHIQL = settings.DEBUG or getattr(
-    settings, "BIFROST_EXPOSE_GRAPHIQL", False
-)
-urlpatterns = [url(r"^graphql", csrf_exempt(FileUploadGraphQLView.as_view()))]
+urlpatterns = [url(r"^graphql", csrf_exempt(BifrostView.as_view()))]
 
 websocket_urlpatterns = [
-    path("subscription/", GraphqlSubscriptionConsumer),
+    path("graphql/", GraphqlSubscriptionConsumer),
 ]
 
-if SHOULD_EXPOSE_GRAPHIQL:
-    urlpatterns.append(url(r"^graphiql", graphiql))
+# if SHOULD_EXPOSE_GRAPHIQL:
+#     urlpatterns.append(url(r"^graphiql", graphiql))
