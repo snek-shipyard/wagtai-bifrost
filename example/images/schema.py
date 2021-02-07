ab22bca733715510ad03428800233ebf335726b3
longwand <longwand@schett.net>
2021-02-07 15:34:14 +0100

    Add example images schema
    The example mutation has been modified.
    A example query has been added.

--- example/images/schema.py
+++ example/images/schema.py
@@ -1,16 +1,33 @@
+from bifrost.registry import registry
 import graphene
+from .models import CustomImage
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
 class Test(graphene.Mutation):
-    member = graphene.String()
+    member = graphene.Field(
+        registry.models[get_user_model()], token=graphene.String(required=False)
+    )
     generated_password = graphene.String()
 
     class Arguments:
         token = graphene.String(required=False)
 
     def mutate(self, info, **kwargs):
-        return Test(member="member", generated_password="password")
+        return Test(
+            member=get_user_model().objects.first(), generated_password="password"
+        )
 
 
 class Mutation(graphene.ObjectType):
     test = Test.Field()
+
+
+class Query(graphene.ObjectType):
+    test = graphene.Field(
+        registry.models[get_user_model()], token=graphene.String(required=False)
+    )
+
+    def resolve_test(self, info, **kwargs):
+        return get_user_model().objects.first()
