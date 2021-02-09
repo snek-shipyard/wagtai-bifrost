f7afb68dac6cdb58891b011174958b91bc08dd64
longwand <longwand@schett.net>
2021-02-09 04:33:45 +0100

    Add BifrostFile model with admin registration
    - A model has been introduced which contains a file and its access
    token. The file should be only accessible if the token is valid.
    
    - The Bifrost/File has been added to the model admin.
    
    - `PrivateFileField` from `django-private-storage` is used to ensure
    privacy for internal Bifrost files.

---
+++ bifrost/admin.py
@@ -0,0 +1,31 @@
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    ModelAdminGroup,
+    modeladmin_register,
+)
+
+from .files import BifrostFile
+
+
+class BifrostFileAdmin(ModelAdmin):
+    model = BifrostFile
+    menu_label = "Files"
+    menu_icon = "fa-file"
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+
+
+class BifrostAdmin(ModelAdminGroup):
+    menu_label = "Bifrost"
+    menu_icon = "fa-circle-o-notch"
+    menu_order = 110
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    items = (BifrostFileAdmin,)
+
+
+modeladmin_register(BifrostAdmin)
+
+# SPDX-License-Identifier: (EUPL-1.2)
+# Copyright © 2019-2020 Simon Prast
