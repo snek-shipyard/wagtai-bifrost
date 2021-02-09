bf5b565f11d72961e483089574de86aab421bb71
longwand <longwand@schett.net>
2021-02-09 04:38:09 +0100

    Add BifrostFileView
    - A new view has been added which is used by the private file storage.
    - The view has been added to the bifrost routes.
    
    If superuser or in hand of the correct token, a private file can be downloaded.
    
    `curl http://token@localhost:8000/bifrost-internal/file.txt`

--- bifrost/views.py
+++ bifrost/views.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.conf import settings
 from django.http import Http404
 from django.shortcuts import render
@@ -5,6 +7,9 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
 from graphene_file_upload.django import FileUploadGraphQLView
+from private_storage.views import PrivateStorageView
+
+from .files import BifrostFile
 
 SHOULD_EXPOSE_GRAPHIQL = settings.DEBUG or getattr(
     settings, "BIFROST_EXPOSE_GRAPHIQL", False
@@ -28,3 +33,36 @@ class BifrostView(View):
     @method_decorator(ensure_csrf_cookie)
     def post(self, request):
         return FileUploadGraphQLView.as_view(graphiql=False)(request)
+
+
+class BifrostFileView(PrivateStorageView):
+    model = BifrostFile
+    model_file_field = "file"
+
+    def can_access_file(self, private_file):
+        # Bypass if superuser
+        if self.request.user.is_superuser:
+            return True
+
+        # Get AUTH header
+        if "HTTP_AUTHORIZATION" in self.request.META:
+            auth = self.request.META["HTTP_AUTHORIZATION"]
+            auth = auth.split(" ")
+
+            if len(auth) == 2:
+                if auth[0].lower() == "basic":
+                    access_token = (
+                        base64.b64decode(auth[1]).decode("utf-8").split(":")[0]
+                    )
+
+                    try:
+                        BifrostFile.objects.get(
+                            access_token=access_token, file=private_file.relative_name
+                        )
+
+                        return True
+
+                    except BifrostFile.DoesNotExist:
+                        return False
+
+        return False
